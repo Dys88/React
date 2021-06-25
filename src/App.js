dbb5cc94e8dd12c40d6d7907628a5bdeb10f6f8e
Dys88 <Divyas@shilshatech.com>
2021-06-25 21:01:00 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
+const App = () => {
+  {/* Setting the inital value & updated value as num & set num */}
+  const [num,setNum]= useState(0);
+  {/* For Addition */}
+  const addNum = () => {
+      setNum (num+1);
+  };
+  {/* For Substraction */}
+  const subNum = () => {
+      setNum (num-1);  
+  };
+  {/* For Increament */}
+  const plusNum = () => {
+      setNum (num + 5);  
+  };
+  {/* For Reset */}
+  const resetNum = () => {
+      setNum (0);  
+  };
+   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        {/* Display digit */}
+        <h1> {num}  </h1>
+        {/* Click Add button */}
+        <button onClick={addNum} > Addition </button>
+        {/* Click Sub button */}
+        <button  onClick={subNum} > Subtract </button>
+        {/* Click increament button */}
+        <button  onClick={plusNum} > Increase by 5 </button>
+        {/* Click reset button */}
+        <button  onClick={resetNum} > Reset </button>
+        </div>
       </header>
     </div>
   );
 }
-
 export default App;
